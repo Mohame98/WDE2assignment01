202983d6c4eae3fe3f24edcbc4958e0146603eef
mohame98 <wtafroach@gmail.com>
2024-05-18 17:54:55 -0400

    final commit

--- assignment1.sql
+++ assignment1.sql
@@ -4,6 +4,23 @@ CREATE TABLE client (
     email TEXT
 );
 
+INSERT INTO client (client_id, name, email) VALUES 
+(1, 'JO HN',   'john@mail.com'),
+(2, 'jade', 'jade@mail.comm'),
+(3, 'jillian', 'jillian@milan.com');
+
+SELECT * FROM client;
+
+-- using trim
+SELECT *, TRIM(email) as trimEmail FROM client;
+
+-- using replace
+SELECT name, REPLACE(name, ' ', '') AS replaceName FROM client;
+SELECT email, REPLACE(email, 'milan', 'mail') AS replaceEmail FROM client;
+
+-- using lower
+SELECT name, lower(name) FROM client;
+
 
 
 
--- assignment1readme.md
+++ assignment1readme.md
@@ -2,3 +2,11 @@ MY FUNCTIONS
 
 TRIM, REPLACE, LOWER
 
+TRIM: function is supposed to trim any empty space from the value inserted
+to the database, will demonstrate with value id 1
+
+REPLACE: this can replace any values which i will demonstrate using and 
+jillian@milan.com and john
+
+LOWER: makes all characters in value lower case will demonstrate using john again
+without inserting info
